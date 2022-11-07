c2bed2a4409aa83b34f55d87b3d3ea0209cbb624
Matthew Market <matthewamarket@gmail.com>
2022-11-07 17:09:09 +0000

    Removed customers cte from customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -3,17 +3,6 @@
 )}}
 
 
-with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
-),
-
 orders as (
 
     select
